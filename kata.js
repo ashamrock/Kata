22b8b8c1cc3d14573b6e03d550fdfea08ec4b0a1
A Shamrock <a.shamrock4@gmail.com>
2020-03-13 15:44:28 -0400

    Add files via upload
    started with a method of input for first variable

--- kata.js
+++ kata.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 
+var firstNumber = ""  
+
+firstVariable()
+
+// displays numbers used for first variable 
+function firstVariable(){
+  document.onkeyup = function (event) { 
+    if (event.key >= 0) { 
+      firstNumber += event.key
+      document.getElementById('first-number').innerHTML = firstNumber;
+    } 
+  }
+}
+
+
 });
