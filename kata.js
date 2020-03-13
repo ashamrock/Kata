eaae251f4fc962e582d6523bd0fd6787c916476f
A Shamrock <a.shamrock4@gmail.com>
2020-03-13 16:13:44 -0400

    Add files via upload
    added second variable input based off of first variable input code

--- kata.js
+++ kata.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 var firstNumber = ""  
 var operator = ""
+var secondNumber = ""
 
 firstVariable()
 
@@ -10,13 +11,22 @@ function firstVariable(){
   document.onkeyup = function (event) { 
     if (event.key >= 0) { 
       firstNumber += event.key
-      console.log(event.key)
       document.getElementById('first-number').innerHTML = firstNumber;
     } // Displays operator when pressed
     else if (event.key === "*"||"/"||"+"||"-"){
       operator = event.key
-      console.log(event.key)
       document.getElementById('operator').innerHTML = operator
+      secondVariable()
+    }
+  }
+}
+
+// Displays numbers used for second variable 
+function secondVariable(){
+  document.onkeyup = function (event) { 
+    if (event.key >= 0) { 
+      secondNumber += event.key
+      document.getElementById('second-number').innerHTML = secondNumber;
     }
   }
 }
