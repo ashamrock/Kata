0df19a1c9b5fdb5472d171535bbf4e9e49c9702f
A Shamrock <a.shamrock4@gmail.com>
2020-03-13 16:01:54 -0400

    Add files via upload
    started adding operator

--- kata.js
+++ kata.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
 
 var firstNumber = ""  
+var operator = ""
 
 firstVariable()
 
-// displays numbers used for first variable 
+// Displays numbers used for first variable 
 function firstVariable(){
   document.onkeyup = function (event) { 
     if (event.key >= 0) { 
       firstNumber += event.key
+      console.log(event.key)
       document.getElementById('first-number').innerHTML = firstNumber;
-    } 
+    } // Displays operator when pressed
+    else if (event.key === "+"){
+      operator = event.key
+      console.log(event.key)
+      document.getElementById('operator').innerHTML = operator
+    }
   }
 }
 
